5b2c2aa35406fcf7cf2d7cdd57c7199058168f9d
Nicolae Rosia <nicolae.rosia@gmail.com>
2016-03-27 21:49:45 +0300

    Make it human readable
    Signed-off-by: Nicolae Rosia <nicolae.rosia@gmail.com>

--- src/gazebo_motor_model.cpp
+++ src/gazebo_motor_model.cpp
@@ -141,7 +141,10 @@ void GazeboMotorModel::VelocityCallback(CommandMotorSpeedPtr &rot_velocities) {
   if(rot_velocities->motor_speed_size() < motor_number_) {
     std::cout  << "You tried to access index " << motor_number_
       << " of the MotorSpeed message array which is of size " << rot_velocities->motor_speed_size() << "." << std::endl;
-  } else ref_motor_rot_vel_ = std::min(static_cast<double>(rot_velocities->motor_speed(motor_number_)), static_cast<double>(max_rot_velocity_));
+    return;
+  }
+
+  ref_motor_rot_vel_ = std::min(static_cast<double>(rot_velocities->motor_speed(motor_number_)), static_cast<double>(max_rot_velocity_));
 }
 
 void GazeboMotorModel::UpdateForcesAndMoments() {
