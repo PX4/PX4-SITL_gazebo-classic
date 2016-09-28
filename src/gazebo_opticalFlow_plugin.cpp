df3ee0bc200ee43dc31b47d1c2eb194b1c9c49b9
James Goppert <james.goppert@gmail.com>
2016-09-28 12:51:13 -0400

    Switch flow code to match px4flow.

--- src/gazebo_opticalFlow_plugin.cpp
+++ src/gazebo_opticalFlow_plugin.cpp
@@ -23,13 +23,16 @@
 #include "gazebo/sensors/DepthCameraSensor.hh"
 #include "gazebo_opticalFlow_plugin.h"
 
-#include <cv.h>
 #include <highgui.h>
 #include <math.h>
 #include <string>
 #include <iostream>
 #include <boost/algorithm/string.hpp>
 
+extern "C" {
+  #include "settings.h"
+  #include "flow.h"
+}
 
 using namespace cv;
 using namespace std;
@@ -41,6 +44,8 @@ GZ_REGISTER_SENSOR_PLUGIN(OpticalFlowPlugin)
 OpticalFlowPlugin::OpticalFlowPlugin()
 : SensorPlugin(), width(0), height(0), depth(0), timer_()
 {
+  // load optical flow parameters
+  global_data_reset_param_defaults();
 }
 
 /////////////////////////////////////////////////
@@ -146,62 +151,43 @@ void OpticalFlowPlugin::OnNewFrame(const unsigned char * _image,
 #endif
   const float focal_length = (_width/2)/tan(hfov/2);
 
-#if GAZEBO_MAJOR_VERSION >= 7
-  float rate = this->camera->RenderRate();
-#else
-  float rate = this->camera->GetRenderRate();
-#endif
-  //printf("render rate raw: %10.2f\n", rate);
-  if (!isfinite(rate))
-       rate =  30.0;
-  float dt = 1.0 / rate;
-
-
+  timer_.stop();
+  float rate = 100; // assume 100 hz
+  float dt = 1.0/rate;
   Mat frame = Mat(_height, _width, CV_8UC3);
+  Mat frameHSV = Mat(_height, _width, CV_8UC3);
   Mat frameBGR = Mat(_height, _width, CV_8UC3);
   frame.data = (uchar*)_image; //frame is not the right color now -> convert
-  cvtColor(frame, frameBGR, CV_RGB2BGR); 
+  // dynamically scale image using HSV full conversion
+  cvtColor(frame, frameHSV, CV_RGB2HSV_FULL); 
+  cvtColor(frameHSV, frameBGR, CV_HSV2BGR); 
   cvtColor(frameBGR, frame_gray, CV_BGR2GRAY);
-  
-  featuresPrevious = featuresCurrent;
+  float x_gyro_rate = 0;
+  float y_gyro_rate = 0;
+  float z_gyro_rate = 0;
+  float x_flow = 0;
+  float y_flow = 0;
 
-  goodFeaturesToTrack(frame_gray, featuresCurrent, maxfeatures, qualityLevel, minDistance, Mat(), blockSize, useHarrisDetector, k); //calculate the features
-
-  if (!old_gray.empty() && featuresPrevious.size() > 0){
-    calcOpticalFlowPyrLK(old_gray, frame_gray, featuresPrevious, featuresNextPos, featuresFound, err, cv::Size(8,8), 0);
+  // check for correct image size
+  if (frame_gray.rows != 64 || frame_gray.cols != 64) {
+    printf("incorrect sizes, expected 64x64, got (%dx%d)\n",
+        frame_gray.rows, frame_gray.cols);
+    return;
   }
 
-  /*/// Set the needed parameters to find the refined corners
-  Size winSize = Size( 5, 5 );
-  Size zeroZone = Size( -1, -1 );
-  TermCriteria criteria = TermCriteria( CV_TERMCRIT_EPS + CV_TERMCRIT_ITER, 40, 0.001 );
-
-  // Calculate the refined corner locations
-  cornerSubPix(frame_gray, featuresNextPos, winSize, zeroZone, criteria);
-  cornerSubPix(old_gray, featuresPrevious, winSize, zeroZone, criteria);*/
-
-  //calc diff
-  int n_features = 0;
-  for (int i = 0; i < featuresNextPos.size(); i++) {
-    if (featuresFound[i] == true) {
-        flowX_[i] =  featuresNextPos[i].x - featuresPrevious[i].x;
-        flowY_[i] =  featuresNextPos[i].y - featuresPrevious[i].y;
-        n_features += 1;
-    }       
+  // if no old image yet, create it and return
+  if (old_gray.rows != 64 || old_gray.cols != 64) {
+    old_gray = frame_gray.clone();
+    return;
   }
 
-  float median_flow_x = computeMedian(flowX_, n_features);
-  float median_flow_y = computeMedian(flowY_, n_features);
-
-  float flow_x_ang = atan(median_flow_x/focal_length);
-  float flow_y_ang = atan(median_flow_y/focal_length);
-  uint8_t quality = 255*(float(n_features)/ maxfeatures);
-
+  uint8_t quality = compute_flow((uint8_t *)old_gray.data, (uint8_t * )frame_gray.data,
+      x_gyro_rate, y_gyro_rate, z_gyro_rate, &x_flow, &y_flow);
+  float flow_x_ang = atan(x_flow/focal_length);
+  float flow_y_ang = atan(y_flow/focal_length);
   old_gray = frame_gray.clone();
-
-  //printf("rate: %10.2f f: %10.2f pixel flow x: %10.2f rad\t\tflow y: %10.2f rad\n",
-          //rate, focal_length, flow_x_ang, flow_y_ang);
-
+  //printf("x_flow: %10.4f y_flow: %10.4f rate: %10.2f f: %10.2f pixel flow x: %10.2f rad\t\tflow y: %10.2f rad\n",
+          //x_flow, y_flow, rate, focal_length, flow_x_ang, flow_y_ang);
   opticalFlow_message.set_time_usec(100000000000000);//big number to prevent timeout in inav
   opticalFlow_message.set_sensor_id(2.0);
   opticalFlow_message.set_integration_time_us(dt * 1000000);
@@ -211,43 +197,12 @@ void OpticalFlowPlugin::OnNewFrame(const unsigned char * _image,
   opticalFlow_message.set_integrated_ygyro(0.0); //get real values in gazebo_mavlink_interface.cpp
   opticalFlow_message.set_integrated_zgyro(0.0); //get real values in gazebo_mavlink_interface.cpp
   opticalFlow_message.set_temperature(20.0);
-  opticalFlow_message.set_quality(quality); //features?
+  opticalFlow_message.set_quality(quality);
   opticalFlow_message.set_time_delta_distance_us(0.0);
   opticalFlow_message.set_distance(0.0); //get real values in gazebo_mavlink_interface.cpp
 
   opticalFlow_pub_->Publish(opticalFlow_message);
-
-
-  // TODO handle case when more than one vehicles are using the plugin, then
-  // update rate will be n_vehicle*rate, need to store in vehicle structure somehow.
-  timer_.stop();
-  float rate_real = 1.0e9/timer_.elapsed().wall;
-  if (rate_real < 0.75*rate) {
-    printf("!!! gazebo opticaal flow plugin running slow: %10.0f Hz\n", rate_real);
-  }
   timer_.start();
 }
-
-float OpticalFlowPlugin::computeMedian(float * array, int iSize) {
-    for (int i = iSize - 1; i > 0; --i) {
-        for (int j = 0; j < i; ++j) {
-            if (array[j] > array[j+1]) {
-                float dTemp = array[j];
-                array[j] = array[j+1];
-                array[j+1] = dTemp;
-            }
-        }
-    }
-
-    // Middle or average of middle values in the sorted array.
-    float dMedian = 0.0;
-    if ((iSize % 2) == 0) {
-        dMedian = (array[iSize/2] + array[(iSize/2) - 1])/2.0;
-    } else {
-        dMedian = array[iSize/2];
-    }
-    return dMedian;
-}
-
 
 /* vim: set et fenc=utf-8 ff=unix sts=0 sw=2 ts=2 : */
