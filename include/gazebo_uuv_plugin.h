f4eb52704f03633ae45f6bde11fc8ca761e5e938
NRottmann <Nils.Rottmann@tuhh.de>
2017-10-05 10:24:33 +0200

    Change Absolute Value Method

--- include/gazebo_uuv_plugin.h
+++ include/gazebo_uuv_plugin.h
@@ -67,8 +67,6 @@ class GazeboUUVPlugin : public ModelPlugin {
     void CommandCallback(CommandMotorSpeedPtr &command);
     double command_[4];
 
-    double absolute(double x);
-
     double last_time_;
     double time_delta_;
     
