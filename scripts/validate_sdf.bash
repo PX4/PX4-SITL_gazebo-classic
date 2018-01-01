a85a43a13768175d942669d44d26db699948035a
TSC21 <n.marques21@hotmail.com>
2018-01-01 22:07:10 +0000

    validate_sdf: ignore delta_wing SDF for now; add possibility for using another schema

--- scripts/validate_sdf.bash
+++ scripts/validate_sdf.bash
@@ -3,9 +3,13 @@
 # Pass the dirs of the models by command line args; if not set, defaults are used
 MODELS_DIR=${1:-"../models"}
 WORLDS_DIR=${2:-"../worlds"}
+# Pass another schema as a 3rd argument, if one wants to use a customized or local schema
+# Default: http://sdformat.org/schemas/root.xsd
+SCHEMA=${3:-"http://sdformat.org/schemas/root.xsd"}
 
 # Validate the models SDF schemas according to http://sdformat.org/schemas/root.xsd format
-find ${MODELS_DIR} -type f  -name '*.sdf' -exec xmllint --format -schema http://sdformat.org/schemas/root.xsd {} --noout \;
+# Note: ignoring delta_wing.sdf as the SDFormat schema doesn't currently consider Xacro xmlns declarations
+find ${MODELS_DIR} -type f -name '*.sdf' ! -name 'delta_wing.sdf' -exec xmllint --schema ${SCHEMA} {} --noout \;
 
 # Validate the models SDF schemas according to http://sdformat.org/schemas/root.xsd format
-find ${WORLDS_DIR} -type f  -name '*.world' -exec xmllint --format -schema http://sdformat.org/schemas/root.xsd {} --noout \;
+find ${WORLDS_DIR} -type f -name '*.world' -exec xmllint --schema ${SCHEMA} {} --noout \;
