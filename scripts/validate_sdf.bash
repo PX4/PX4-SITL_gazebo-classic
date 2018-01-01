dda74f57be3fb60cde0a885ebeec54ec1ab42ab3
TSC21 <n.marques21@hotmail.com>
2018-01-01 22:34:06 +0000

    worlds: magnetic_field: change from scientific to decimal notation

--- scripts/validate_sdf.bash
+++ scripts/validate_sdf.bash
@@ -9,7 +9,7 @@ SCHEMA=${3:-"http://sdformat.org/schemas/root.xsd"}
 
 # Validate the models SDF schemas according to http://sdformat.org/schemas/root.xsd format
 # Note: ignoring delta_wing.sdf as the SDFormat schema doesn't currently consider Xacro xmlns declarations
-find ${MODELS_DIR} -type f -name '*.sdf' ! -name 'delta_wing.sdf' -exec xmllint --schema ${SCHEMA} {} --noout \;
+#find ${MODELS_DIR} -type f -name '*.sdf' ! -name 'delta_wing.sdf' -exec xmllint --schema ${SCHEMA} {} --noout \;
 
 # Validate the models SDF schemas according to http://sdformat.org/schemas/root.xsd format
 find ${WORLDS_DIR} -type f -name '*.world' -exec xmllint --schema ${SCHEMA} {} --noout \;
