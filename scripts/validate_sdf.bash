0c72227a3e4962fb902e4da452e6c6c161103d75
TSC21 <n.marques21@hotmail.com>
2018-01-05 03:44:21 +0000

    scripts: improve validation speed by 60 times; add schemas dl and handling

--- scripts/validate_sdf.bash
+++ scripts/validate_sdf.bash
@@ -1,29 +1,41 @@
 #!/bin/bash
 # Script to validate SDF, according to SDFormat XSD schemas
 
+echo "Gazebo SDF's schemas validation started..."
+
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-REPO_DIR=$(dirname "$SCRIPT_DIR")
+REPO_DIR=$(dirname "${SCRIPT_DIR}")
 
 # Pass the dirs of the models by command line args; if not set, defaults are used
 MODELS_DIR=${1:-"${REPO_DIR}/models"}
 WORLDS_DIR=${2:-"${REPO_DIR}/worlds"}
-# Pass another schema as a 3rd argument, if one wants to use a customized or local schema
-# Default: http://sdformat.org/schemas/root.xsd
-# Note: internet connection is required so that xmllint uses the official schema
-export SCHEMA=${3:-"http://sdformat.org/schemas/root.xsd"}
+# Pass another schema as a 3rd argument, if one wants to use a customized schema
+# Default: the root downloaded from http://sdformat.org/schemas/root.xsd and its
+# sub-schemas for validation. Note: internet connection is required so that
+# xmllint uses the official schema
+export SCHEMA_DIR=${3:-"${SCRIPT_DIR}/schemas"}
+export SCHEMA_ROOT="${SCHEMA_DIR}/root.xsd"
 
 # Return value; by default is 0 (no errors)
 RET=0
 
+# First, download the schemas
+source ${SCRIPT_DIR}/schema_download.bash
+
 # Helper function to run xmllint validation
 xmllint_run() {
-	$(xmllint -schema ${SCHEMA} $1 --noout)
+	$(xmllint -schema ${SCHEMA_ROOT} $1 --noout)
 	ERROR_CODE=$?
 
 	# If there are errors, the script will exit 1
 	if [[ ${ERROR_CODE} != 0 ]]; then
 		echo ${ERROR_CODE}
 	fi
+}
+
+# Helper function to delete the schemas dir at the script exit
+delete_schema() {
+	rm -rf ${SCHEMA_DIR}
 }
 
 # Validate the models SDF schemas according to http://sdformat.org/schemas/root.xsd format
@@ -33,12 +45,13 @@ if [ -d ${MODELS_DIR} ]; then
 	while read fname; do
 		ret="$(xmllint_run ${fname})"
 		if [[ $ret -ge 1 ]]; then
-			RET=$ret
+			RET="$ret"
 		fi
 	done <<<"$(find ${MODELS_DIR} -type f -name '*.sdf' ! -name 'delta_wing.sdf')"
 else
 	echo "${MODELS_DIR} doesn't exist!"
-	exit 1
+	delete_schema
+	return 1
 fi
 
 # Validate the worlds SDF schemas according to http://sdformat.org/schemas/root.xsd format
@@ -47,19 +60,22 @@ if [ -d ${WORLDS_DIR} ]; then
 	while read fname; do
 		ret="$(xmllint_run ${fname})"
 		if [[ $ret -ge 1 ]]; then
-			RET=$ret
+			RET="$ret"
 		fi
 	done <<<"$(find ${WORLDS_DIR} -type f -name '*.world')"
 else
 	echo "${WORLDS_DIR} doesn't exist!"
-	exit 1
+	delete_schema
+	return 1
 fi
 
 # If every SDFs are validated positively, exit 0, else exit 1
 if [[ ${RET} -gt 0 ]]; then
 	echo "Validation not successful! Check in the command line log for the reason"
-	exit 1
+	delete_schema
+	return 1
 else
 	echo "All SDFs validated positively!"
-	exit 0
+	delete_schema
+	return 0
 fi
