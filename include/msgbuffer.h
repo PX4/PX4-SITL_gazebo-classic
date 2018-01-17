b4ff8a623e746b74e8346aeb4666f9b60252c0e4
amy wagoner <arwagoner@gmail.com>
2018-01-17 15:52:25 -0500

    Changed license

--- include/msgbuffer.h
+++ include/msgbuffer.h
@@ -4,13 +4,20 @@
  * @author Vladimir Ermakov <vooon341@gmail.com>
  * @author Amy Wagoner <arwagoner@gmail.com> (adaptation to Gazebo)
  */
-/*
- * libmavconn
- * Copyright 2014,2015,2016 Vladimir Ermakov, All rights reserved.
+/**
+ * Copyright 2017 MAVROS dev team. All rights reserved.
  *
- * This file is part of the mavros package and subject to the license terms
- * in the top-level LICENSE file of the mavros repository.
- * https://github.com/mavlink/mavros/tree/master/LICENSE.md
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 #pragma once
