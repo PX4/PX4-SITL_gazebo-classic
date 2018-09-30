3b3ba997ca69c9925e951baea2810034e9dee6a4
TSC21 <n.marques21@hotmail.com>
2018-09-30 01:23:58 +0100

    gazebo_motor_failure_plugin: clean up and fix for Gazebo 9

--- src/gazebo_motor_failure_plugin.cpp
+++ src/gazebo_motor_failure_plugin.cpp
@@ -15,31 +15,52 @@
  * limitations under the License.
  */
 
-#include "gazebo_motor_failure_plugin.h"
+#include <gazebo_motor_failure_plugin.h>
 
 namespace gazebo {
 
-GazeboMotorFailure::GazeboMotorFailure() : ModelPlugin(), ROS_motor_num_sub_topic_(kDefaultROSMotorNumSubTopic), motor_failure_num_pub_topic_(kDefaultMotorFailureNumPubTopic) {
-}
+GazeboMotorFailure::GazeboMotorFailure() :
+    ModelPlugin(),
+    ROS_motor_num_sub_topic_(kDefaultROSMotorNumSubTopic),
+    motor_failure_num_pub_topic_(kDefaultMotorFailureNumPubTopic)
+{ }
 
 GazeboMotorFailure::~GazeboMotorFailure() {
-  event::Events::DisconnectWorldUpdateBegin(updateConnection_);
+  this->updateConnection_.reset();
+
+  this->rosQueue.clear();
+  this->rosQueue.disable();
+  this->rosNode->shutdown();
+  this->rosQueueThread.join();
+
+  delete this->rosNode;
 }
 
 void GazeboMotorFailure::Load(physics::ModelPtr _model, sdf::ElementPtr _sdf) {
 
-  namespace_.clear();
+  this->namespace_.clear();
 
-  node_handle_ = transport::NodePtr(new transport::Node());
-  node_handle_->Init(namespace_);
+  if (_sdf->HasElement("robotNamespace"))
+    this->namespace_ = _sdf->GetElement("robotNamespace")->Get<std::string>() + "/";
 
   motor_failure_pub_ = node_handle_->Advertise<msgs::Int>(motor_failure_num_pub_topic_, 1);
 
-  getSdfParam<std::string>(_sdf, "ROSMotorNumSubTopic", ROS_motor_num_sub_topic_, ROS_motor_num_sub_topic_);
-  getSdfParam<std::string>(_sdf, "MotorFailureNumPubTopic", motor_failure_num_pub_topic_, motor_failure_num_pub_topic_);
+  if (!_sdf->HasElement("ROSMotorNumSubTopic")) {
+    ROS_FATAL_NAMED("MotorFailure", "MotorFailure plugin missing <ROSMotorNumSubTopic>, cannot proceed");
+    return;
+  }
+  else
+    this->ROS_motor_num_sub_topic_ = _sdf->GetElement("ROSMotorNumSubTopic")->Get<std::string>();
+
+  if (!_sdf->HasElement("MotorFailureNumPubTopic")) {
+    ROS_FATAL_NAMED("MotorFailure", "MotorFailure plugin missing <MotorFailureNumPubTopic>, cannot proceed");
+    return;
+  }
+  else
+    this->motor_failure_num_pub_topic_ = _sdf->GetElement("MotorFailureNumPubTopic")->Get<std::string>();
 
   // ROS Topic subscriber
-  // Initialize ROS, if it has not already bee initialized.
+  // Initialize ROS, if it has not already been initialized.
   if (!ros::isInitialized())  {
     int argc = 0;
     char **argv = NULL;
@@ -47,37 +68,38 @@ void GazeboMotorFailure::Load(physics::ModelPtr _model, sdf::ElementPtr _sdf) {
   }
 
   // Create our ROS node. This acts in a similar manner to the Gazebo node
-  this->rosNode.reset(new ros::NodeHandle("gazebo_client"));
+  this->rosNode = new ros::NodeHandle(this->namespace_);
 
   // Create a named topic, and subscribe to it.
-  ros::SubscribeOptions so = ros::SubscribeOptions::create<std_msgs::Int32>(ROS_motor_num_sub_topic_, 1, boost::bind(&GazeboMotorFailure::motorFailNumCallBack, this, _1), ros::VoidPtr(), &this->rosQueue);
+  ros::SubscribeOptions so = ros::SubscribeOptions::create<std_msgs::Int32>(ROS_motor_num_sub_topic_,
+        1,
+        boost::bind(&GazeboMotorFailure::motorFailNumCallBack, this, _1),
+        ros::VoidPtr(),
+        &this->rosQueue);
   this->rosSub = this->rosNode->subscribe(so);
-  
+
   this->rosQueueThread = std::thread(std::bind(&GazeboMotorFailure::QueueThread, this));
 
   std::cout << "[gazebo_motor_failure_plugin]: Subscribe to ROS topic: "<< ROS_motor_num_sub_topic_ << std::endl;
 
   // Listen to the update event. This event is broadcast every
   // simulation iteration.
-  updateConnection_ = event::Events::ConnectWorldUpdateBegin(boost::bind(&GazeboMotorFailure::OnUpdate, this, _1));
+  this->updateConnection_ = event::Events::ConnectWorldUpdateBegin(
+    boost::bind(&GazeboMotorFailure::OnUpdate, this, _1));
 }
 
-void GazeboMotorFailure::OnUpdate(const common::UpdateInfo& _info) {
-  Publish_num();
+void GazeboMotorFailure::OnUpdate(const common::UpdateInfo &info) {
+    this->motor_failure_msg_.set_data(motor_Failure_Number_);
+    this->motor_failure_pub_->Publish(motor_failure_msg_);
 }
 
-void GazeboMotorFailure::motorFailNumCallBack(const std_msgs::Int32ConstPtr& _msg) {
-  this->motor_Failure_Number_ = _msg->data;
-  //std::cout << "[gazebo_motor_failure_plugin]: Subscribe to " << ROS_motor_num_sub_topic_ << std::endl;
-}
-
-void GazeboMotorFailure::Publish_num() {
-  motor_failure_msg_.set_data(motor_Failure_Number_);
-  motor_failure_pub_->Publish(motor_failure_msg_);
+void GazeboMotorFailure::motorFailNumCallBack(const std_msgs::Int32ConstPtr &msg) {
+  this->motor_Failure_Number_ = msg->data;
 }
 
 void GazeboMotorFailure::QueueThread() {
   static const double timeout = 0.01;
+
   while (this->rosNode->ok()) {
     this->rosQueue.callAvailable(ros::WallDuration(timeout));
   }
@@ -85,4 +107,3 @@ void GazeboMotorFailure::QueueThread() {
 
 GZ_REGISTER_MODEL_PLUGIN(GazeboMotorFailure);
 }
-
