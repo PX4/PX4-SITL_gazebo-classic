d08eb5b22d3edfb7a740c883b06f895a173b5519
Julian Oes <julian@oes.ch>
2018-11-27 13:40:19 +0100

    Update opencv includes
    Starting with opencv 4.0 the include cv.h is no longer available.
    Therefore, the deprecated includes are removed and replaced with the
    opencv2 includes.
    
    Also, one the define RGB2BGR needed updating.

--- src/gazebo_geotagged_images_plugin.cpp
+++ src/gazebo_geotagged_images_plugin.cpp
@@ -21,10 +21,6 @@
 #include <string>
 #include <iostream>
 #include <boost/filesystem.hpp>
-
-#include <cv.h>
-#include <highgui.h>
-
 #include <opencv2/opencv.hpp>
 
 using namespace std;
@@ -192,7 +188,7 @@ void GeotaggedImagesPlugin::OnNewFrame(const unsigned char * image)
     Mat frame    = Mat(_height, _width, CV_8UC3);
     Mat frameBGR = Mat(_height, _width, CV_8UC3);
     frame.data   = (uchar*)image; //frame has not the right color format yet -> convert
-    cvtColor(frame, frameBGR, CV_RGB2BGR);
+    cvtColor(frame, frameBGR, COLOR_RGB2BGR);
 
     char file_name[256];
     snprintf(file_name, sizeof(file_name), "%s/DSC%05i.jpg", _storageDir.c_str(), _imageCounter);
