7f1b8e2512fa37452c186aadecdfa0f1097b3e01
JaeyoungLim <jaeyoung@auterion.com>
2020-05-08 11:09:54 +0200

    Fix gazebo motor model plugin calculations (#484)
    * Fix gazebo motor model
    
    Dot product was being calculated as a element wise product
    
    * Update src/gazebo_motor_model.cpp
    
    Co-authored-by: Mathieu Bresciani <brescianimathieu@gmail.com>
    
    Co-authored-by: Mathieu Bresciani <brescianimathieu@gmail.com>

--- src/gazebo_motor_model.cpp
+++ src/gazebo_motor_model.cpp
@@ -200,9 +200,9 @@ void GazeboMotorModel::UpdateForcesAndMoments() {
   // XXX this has to be modelled better
   //
 #if GAZEBO_MAJOR_VERSION >= 9
-  ignition::math::Vector3d body_velocity = link_->WorldLinearVel() - wind_vel_;
+  ignition::math::Vector3d body_velocity = link_->WorldLinearVel();
 #else
-  ignition::math::Vector3d body_velocity = ignitionFromGazeboMath(link_->GetWorldLinearVel())  - wind_vel_;
+  ignition::math::Vector3d body_velocity = ignitionFromGazeboMath(link_->GetWorldLinearVel());
 #endif
   double vel = body_velocity.Length();
   double scalar = 1 - vel / 25.0; // at 50 m/s the rotor will not produce any force anymore
@@ -219,8 +219,8 @@ void GazeboMotorModel::UpdateForcesAndMoments() {
 #else
   ignition::math::Vector3d joint_axis = ignitionFromGazeboMath(joint_->GetGlobalAxis(0));
 #endif
-  //ignition::math::Vector3d body_velocity = link_->WorldLinearVel();
-  ignition::math::Vector3d body_velocity_perpendicular = body_velocity - (body_velocity * joint_axis) * joint_axis;
+  ignition::math::Vector3d relative_wind_velocity = body_velocity - wind_vel_;
+  ignition::math::Vector3d body_velocity_perpendicular = relative_wind_velocity - (relative_wind_velocity.Dot(joint_axis)) * joint_axis;
   ignition::math::Vector3d air_drag = -std::abs(real_motor_velocity) * rotor_drag_coefficient_ * body_velocity_perpendicular;
   // Apply air_drag to link.
   link_->AddForce(air_drag);
