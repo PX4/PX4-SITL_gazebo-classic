629e5f5c239d88421edab12b4a7e2e3c3ea7e8fc
Morten Fyhn Amundsen <morten.fyhn.amundsen@gmail.com>
2020-07-15 09:02:44 +0200

    Barometer drift (#544)
    * gazebo_barometer_plugin: Refactor slightly
    
    * Make constant stuff const.
    * Avoid re-assigning to the same variable.
    * Other small refactors.
    
    * gazebo_barometer_plugin: Implement linear drift
    
    A new plugin argument 'baro_drift_pa_per_sec'
    lets you configure a linear drift in Pascal
    per second. Will cause the barometer measured
    pressure to increase/decrease linearly forever.

--- src/gazebo_barometer_plugin.cpp
+++ src/gazebo_barometer_plugin.cpp
@@ -46,7 +46,8 @@ GZ_REGISTER_MODEL_PLUGIN(BarometerPlugin)
 
 BarometerPlugin::BarometerPlugin() : ModelPlugin(),
     baro_rnd_y2_(0.0),
-    baro_rnd_use_last_(false)
+    baro_rnd_use_last_(false),
+    baro_drift_pa_(0.0)
 {
 }
 
@@ -85,6 +86,12 @@ void BarometerPlugin::getSdfParams(sdf::ElementPtr sdf)
     baro_topic_ = kDefaultBarometerTopic;
     gzwarn << "[gazebo_barometer_plugin] Using default barometer topic " << baro_topic_ << "\n";
   }
+
+  if (sdf->HasElement("baroDriftPaPerSec")) {
+    baro_drift_pa_per_sec_ = sdf->GetElement("baroDriftPaPerSec")->Get<double>();
+  } else {
+    baro_drift_pa_per_sec_ = 0.0;
+  }
 }
 
 void BarometerPlugin::Load(physics::ModelPtr model, sdf::ElementPtr sdf)
@@ -119,67 +126,73 @@ void BarometerPlugin::Load(physics::ModelPtr model, sdf::ElementPtr sdf)
 void BarometerPlugin::OnUpdate(const common::UpdateInfo&)
 {
 #if GAZEBO_MAJOR_VERSION >= 9
-  common::Time current_time = world_->SimTime();
+  const common::Time current_time = world_->SimTime();
 #else
-  common::Time current_time = world_->GetSimTime();
+  const common::Time current_time = world_->GetSimTime();
 #endif
-  double dt = (current_time - last_pub_time_).Double();
+  const double dt = (current_time - last_pub_time_).Double();
 
   if (dt > 1.0 / pub_rate_) {
 
     // get pose of the model that the plugin is attached to
 #if GAZEBO_MAJOR_VERSION >= 9
-    ignition::math::Pose3d pose_model_world = model_->WorldPose();
+    const ignition::math::Pose3d pose_model_world = model_->WorldPose();
 #else
-    ignition::math::Pose3d pose_model_world = ignitionFromGazeboMath(model_->GetWorldPose());
+    const ignition::math::Pose3d pose_model_world = ignitionFromGazeboMath(model_->GetWorldPose());
 #endif
     ignition::math::Pose3d pose_model; // Z-component pose in local frame (relative to where it started)
     pose_model.Pos().Z() = pose_model_world.Pos().Z() - pose_model_start_.Pos().Z();
 
-    float pose_n_z = -pose_model.Pos().Z(); // convert Z-component from ENU to NED
+    const float pose_n_z = -pose_model.Pos().Z(); // convert Z-component from ENU to NED
 
     // calculate abs_pressure using an ISA model for the tropsphere (valid up to 11km above MSL)
     const float lapse_rate = 0.0065f; // reduction in temperature with altitude (Kelvin/m)
     const float temperature_msl = 288.0f; // temperature at MSL (Kelvin)
-    float alt_msl = (float)alt_home_ - pose_n_z;
-    float temperature_local = temperature_msl - lapse_rate * alt_msl;
-    float pressure_ratio = powf((temperature_msl/temperature_local), 5.256f);
+    const float alt_msl = (float)alt_home_ - pose_n_z;
+    const float temperature_local = temperature_msl - lapse_rate * alt_msl;
+    const float pressure_ratio = powf(temperature_msl / temperature_local, 5.256f);
     const float pressure_msl = 101325.0f; // pressure at MSL
-    float absolute_pressure = pressure_msl / pressure_ratio;
+    const float absolute_pressure = pressure_msl / pressure_ratio;
 
     // generate Gaussian noise sequence using polar form of Box-Muller transformation
-    double x1, x2, w, y1;
-    if (!baro_rnd_use_last_) {
-      do {
-        x1 = 2.0 * standard_normal_distribution_(random_generator_) - 1.0;
-        x2 = 2.0 * standard_normal_distribution_(random_generator_) - 1.0;
-        w = x1 * x1 + x2 * x2;
-      } while ( w >= 1.0 );
-      w = sqrt( (-2.0 * log( w ) ) / w );
-      // calculate two values - the second value can be used next time because it is uncorrelated
-      y1 = x1 * w;
-      baro_rnd_y2_ = x2 * w;
-      baro_rnd_use_last_ = true;
-    } else {
-      // no need to repeat the calculation - use the second value from last update
-      y1 = baro_rnd_y2_;
-      baro_rnd_use_last_ = false;
+    double y1;
+    {
+      double x1, x2, w;
+      if (!baro_rnd_use_last_) {
+        do {
+          x1 = 2.0 * standard_normal_distribution_(random_generator_) - 1.0;
+          x2 = 2.0 * standard_normal_distribution_(random_generator_) - 1.0;
+          w = x1 * x1 + x2 * x2;
+        } while ( w >= 1.0 );
+        w = sqrt( (-2.0 * log( w ) ) / w );
+        // calculate two values - the second value can be used next time because it is uncorrelated
+        y1 = x1 * w;
+        baro_rnd_y2_ = x2 * w;
+        baro_rnd_use_last_ = true;
+      } else {
+        // no need to repeat the calculation - use the second value from last update
+        y1 = baro_rnd_y2_;
+        baro_rnd_use_last_ = false;
+      }
     }
 
-    // Apply 1 Pa RMS noise
-    float abs_pressure_noise = 1.0f * (float)y1;
-    absolute_pressure += abs_pressure_noise;
+    // Apply noise and drift
+    const float abs_pressure_noise = 1.0f * (float)y1;  // 1 Pa RMS noise
+    baro_drift_pa_ += baro_drift_pa_per_sec_ * dt;
+    const float absolute_pressure_noisy = absolute_pressure + abs_pressure_noise + baro_drift_pa_;
 
     // convert to hPa
-    absolute_pressure *= 0.01f;
-    baro_msg_.set_absolute_pressure(absolute_pressure);
+    const float absolute_pressure_noisy_hpa = absolute_pressure_noisy * 0.01f;
+    baro_msg_.set_absolute_pressure(absolute_pressure_noisy_hpa);
 
     // calculate density using an ISA model for the tropsphere (valid up to 11km above MSL)
-    const float density_ratio = powf((temperature_msl/temperature_local) , 4.256f);
-    float rho = 1.225f / density_ratio;
+    const float density_ratio = powf(temperature_msl / temperature_local, 4.256f);
+    const float rho = 1.225f / density_ratio;
 
     // calculate pressure altitude including effect of pressure noise
-    baro_msg_.set_pressure_altitude(alt_msl - abs_pressure_noise / (gravity_W_.Length() * rho));
+    baro_msg_.set_pressure_altitude(alt_msl -
+                                    (abs_pressure_noise + baro_drift_pa_) /
+                                        (gravity_W_.Length() * rho));
 
     // calculate temperature in Celsius
     baro_msg_.set_temperature(temperature_local - 273.0f);
