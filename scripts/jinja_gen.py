9d2b19784cb34bf8f397ab2e2f368f682f2d5d5a
JaeyoungLim <jaeyoung@auterion.com>
2020-09-12 23:11:18 +0200

    Use jinja templates for standard_vtol and plane model (#590)
    * Use jinja templates for standard_vtol and plane
    
    Use jinja templates for standard_vtol and plane models
    
    * Remove -gen.sdf suffix from the model configs

--- scripts/jinja_gen.py
+++ scripts/jinja_gen.py
@@ -11,6 +11,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('filename')
     parser.add_argument('env_dir')
+    parser.add_argument('--mavlink_tcp_port', default=4560)
+    parser.add_argument('--mavlink_udp_port', default=14560)
     args = parser.parse_args()
     env = jinja2.Environment(loader=jinja2.FileSystemLoader(args.env_dir))
     template = env.get_template(os.path.relpath(args.filename, args.env_dir))
@@ -23,9 +25,9 @@ if __name__ == "__main__":
         pass
         rospack = None
 
-    d = {'np': np, 'rospack': rospack}
+    d = {'np': np, 'rospack': rospack, 'mavlink_tcp_port': args.mavlink_tcp_port, 'mavlink_udp_port': args.mavlink_udp_port}
     result = template.render(d)
-    filename_out = args.filename.replace('.sdf.jinja', '-gen.sdf')
+    filename_out = args.filename.replace('.sdf.jinja', '.sdf')
     with open(filename_out, 'w') as f_out:
         print(('{:s} -> {:s}'.format(args.filename, filename_out)))
         f_out.write(result)
