31445dee74acc0a09cbcc0415145ce9f88c5a100
JaeyoungLim <jaeyoung@auterion.com>
2020-09-29 13:23:06 +0200

    Support directing outputfile of jinja templates (#612)
    This commit adds directing the output of jinja templates to a specific path. This is needed for multivehicle model generation where the generated sdf files need to be directed to specific paths

--- scripts/jinja_gen.py
+++ scripts/jinja_gen.py
@@ -9,10 +9,11 @@ import numpy as np
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('filename')
+    parser.add_argument('filename', help="file that the sdf file should be generated from")
     parser.add_argument('env_dir')
-    parser.add_argument('--mavlink_tcp_port', default=4560)
-    parser.add_argument('--mavlink_udp_port', default=14560)
+    parser.add_argument('--mavlink_tcp_port', default=4560, help="TCP port for PX4 SITL")
+    parser.add_argument('--mavlink_udp_port', default=14560, help="Mavlink UDP port for mavlink access")
+    parser.add_argument('--output-file', help="sdf output file")
     args = parser.parse_args()
     env = jinja2.Environment(loader=jinja2.FileSystemLoader(args.env_dir))
     template = env.get_template(os.path.relpath(args.filename, args.env_dir))
@@ -27,7 +28,11 @@ if __name__ == "__main__":
 
     d = {'np': np, 'rospack': rospack, 'mavlink_tcp_port': args.mavlink_tcp_port, 'mavlink_udp_port': args.mavlink_udp_port}
     result = template.render(d)
-    filename_out = args.filename.replace('.sdf.jinja', '-gen.sdf')
+    if args.output_file:
+        filename_out = args.output_file
+    else:
+        filename_out = args.filename.replace('.sdf.jinja', '-gen.sdf')
+
     with open(filename_out, 'w') as f_out:
         print(('{:s} -> {:s}'.format(args.filename, filename_out)))
         f_out.write(result)
