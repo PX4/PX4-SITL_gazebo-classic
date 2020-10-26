aa6003ff76eed65b124675b2cb85f0579d56a954
Benjamin Perseghetti <perseghetti@gmail.com>
2020-10-26 02:39:19 -0400

    A complete overhaul of vehicle and world generation through jinja as well as json. Adds features to enable wind, dynamically set sun, clouds, models for hitl, and much more.

--- scripts/jinja_gen.py
+++ scripts/jinja_gen.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python3
-
-
 import jinja2
 import argparse
 import os
@@ -11,12 +9,15 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('filename', help="file that the sdf file should be generated from")
     parser.add_argument('env_dir')
+    parser.add_argument('--sdf_version', default="NotSet", help="SDF format version to use for interpreting file")
     parser.add_argument('--mavlink_tcp_port', default=4560, help="TCP port for PX4 SITL")
     parser.add_argument('--mavlink_udp_port', default=14560, help="Mavlink UDP port for mavlink access")
     parser.add_argument('--serial_enabled', default=0, help="Enable Serial device for HITL")
     parser.add_argument('--serial_device', default="/dev/ttyACM0", help="Serial device for FMU")
     parser.add_argument('--serial_baudrate', default=921600, help="Baudrate of Serial device for FMU")
+    parser.add_argument('--enable_lockstep', default=1, help="Enable lockstep for simulation")
     parser.add_argument('--hil_mode', default=0, help="Enable HIL mode for HITL simulation")
+    parser.add_argument('--model_name', default="NotSet", help="Model to be used in jinja files")
     parser.add_argument('--output-file', help="sdf output file")
     parser.add_argument('--stdout', action='store_true', default=False, help="dump to stdout instead of file")
     args = parser.parse_args()
@@ -32,11 +33,14 @@ if __name__ == "__main__":
         rospack = None
 
     d = {'np': np, 'rospack': rospack, \
+         'sdf_version': args.sdf_version, \
          'mavlink_tcp_port': args.mavlink_tcp_port, \
          'mavlink_udp_port': args.mavlink_udp_port, \
          'serial_enabled': args.serial_enabled, \
          'serial_device': args.serial_device, \
          'serial_baudrate': args.serial_baudrate, \
+         'enable_lockstep': args.enable_lockstep, \
+         'model_name': args.model_name, \
          'hil_mode': args.hil_mode}
 
     result = template.render(d)
