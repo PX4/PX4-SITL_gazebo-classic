dbdbd7ce2cf80c34cb528987611dea098968e87c
Jaeyoung-Lim <jalim@ethz.ch>
2021-09-25 18:58:55 +0200

    Add battery plugin

---
+++ src/gazebo_battery_plugin.cpp
@@ -0,0 +1,95 @@
+/****************************************************************************
+ *
+ *   Copyright (c) 2021 PX4 Development Team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+/**
+ * @brief Drop Plugin
+ *
+ * This plugin simulates dropping a payload
+ *
+ * @author Jaeyoung Lim <jalim@ethz.ch>
+ */
+
+#include "gazebo_battery_plugin.h"
+
+namespace gazebo {
+GZ_REGISTER_MODEL_PLUGIN(BatteryPlugin)
+
+BatteryPlugin::BatteryPlugin() : ModelPlugin()
+{
+}
+
+BatteryPlugin::~BatteryPlugin()
+{
+  update_connection_->~Connection();
+}
+
+void BatteryPlugin::Load(physics::ModelPtr model, sdf::ElementPtr sdf)
+{
+  model_ = model;
+  world_ = model_->GetWorld();
+
+  namespace_.clear();
+  if (sdf->HasElement("robotNamespace")) {
+    namespace_ = sdf->GetElement("robotNamespace")->Get<std::string>();
+  } else {
+    gzerr << "[gazebo_parachute_plugin] Please specify a robotNamespace.\n";
+  }
+
+  if (sdf->HasElement("motorNumber"))
+    motor_number_ = sdf->GetElement("motorNumber")->Get<int>();
+  else
+    gzerr << "[gazebo_parachute_plugin] Please specify a motorNumber.\n";
+
+  getSdfParam<std::string>(sdf, "commandSubTopic", trigger_sub_topic_, trigger_sub_topic_);
+  getSdfParam<std::string>(sdf, "modelName", model_name_, model_name_);
+
+  // Listen to the update event. This event is broadcast every simulation iteration.
+  update_connection_ = event::Events::ConnectWorldUpdateBegin(boost::bind(&BatteryPlugin::OnUpdate, this, _1));
+
+  node_handle_ = transport::NodePtr(new transport::Node());
+  node_handle_->Init(namespace_);
+
+  trigger_sub_ = node_handle_->Subscribe("~/" + model_->GetName() + trigger_sub_topic_, &BatteryPlugin::VelocityCallback, this);
+}
+
+void BatteryPlugin::OnUpdate(const common::UpdateInfo&){
+
+}
+
+void BatteryPlugin::VelocityCallback(CommandMotorSpeedPtr &rot_velocities) {
+  if(rot_velocities->motor_speed_size() < motor_number_) {
+    std::cout  << "You tried to access index " << motor_number_
+      << " of the MotorSpeed message array which is of size " << rot_velocities->motor_speed_size() << "." << std::endl;
+  } else ref_motor_rot_vel_ = std::min(static_cast<double>(rot_velocities->motor_speed(motor_number_)), static_cast<double>(max_rot_velocity_));
+}
+
+} // namespace gazebo
