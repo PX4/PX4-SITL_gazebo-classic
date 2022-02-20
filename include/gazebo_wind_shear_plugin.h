bd23d366260abd893773456162650cf98a414111
Jaeyoung-Lim <jalim@ethz.ch>
2022-02-20 12:43:43 +0100

    Add windshear plugin
    This commit adds a windshear plugin to simulate fixedwing aircraft moving in windshears.
    
    This is useful for testing dynamic soaring

---
+++ include/gazebo_wind_shear_plugin.h
@@ -0,0 +1,136 @@
+/*
+ * Copyright 2015 Fadri Furrer, ASL, ETH Zurich, Switzerland
+ * Copyright 2015 Michael Burri, ASL, ETH Zurich, Switzerland
+ * Copyright 2015 Mina Kamel, ASL, ETH Zurich, Switzerland
+ * Copyright 2015 Janosch Nikolic, ASL, ETH Zurich, Switzerland
+ * Copyright 2015 Markus Achtelik, ASL, ETH Zurich, Switzerland
+ * Copyright 2016 Anton Matosov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#ifndef ROTORS_GAZEBO_PLUGINS_GAZEBO_WIND_PLUGIN_H
+#define ROTORS_GAZEBO_PLUGINS_GAZEBO_WIND_PLUGIN_H
+
+#include <string>
+#include <random>
+#include <gazebo/common/common.hh>
+#include <gazebo/common/Plugin.hh>
+#include <gazebo/gazebo.hh>
+#include <gazebo/physics/physics.hh>
+
+#include "Wind.pb.h"
+
+namespace gazebo {
+// Default values
+static const std::string kDefaultNamespace = "";
+static const std::string kDefaultFrameId = "world";
+
+static constexpr double kDefaultWindVelocityMax = 100.0;
+static constexpr double kDefaultWindVelocityVariance = 0.0;
+static constexpr double kDefaultWindGustVelocityMean = 0.0;
+static constexpr double kDefaultWindGustVelocityMax = 10.0;
+static constexpr double kDefaultWindGustVelocityVariance = 0.0;
+
+static constexpr double kDefaultWindGustStart = 10.0;
+static constexpr double kDefaultWindGustDuration = 0.0;
+
+static const ignition::math::Vector3d kDefaultWindDirectionMean = ignition::math::Vector3d(1, 0, 0);
+static const ignition::math::Vector3d kDefaultWindGustDirectionMean = ignition::math::Vector3d(0, 1, 0);
+static constexpr double kDefaultWindDirectionVariance = 0.0;
+static constexpr double kDefaultWindGustDirectionVariance = 0.0;
+
+/// \brief This gazebo plugin simulates wind acting on a model.
+class GazeboWindShearPlugin : public ModelPlugin {
+ public:
+  GazeboWindShearPlugin()
+      : ModelPlugin(),
+        namespace_(kDefaultNamespace),
+        wind_pub_topic_("world_wind"),
+        wind_velocity_max_(kDefaultWindVelocityMax),
+        wind_velocity_variance_(kDefaultWindVelocityVariance),
+        wind_gust_velocity_mean_(kDefaultWindGustVelocityMean),
+        wind_gust_velocity_max_(kDefaultWindGustVelocityMax),
+        wind_gust_velocity_variance_(kDefaultWindGustVelocityVariance),
+        wind_direction_mean_(kDefaultWindDirectionMean),
+        wind_direction_variance_(kDefaultWindDirectionVariance),
+        wind_gust_direction_mean_(kDefaultWindGustDirectionMean),
+        wind_gust_direction_variance_(kDefaultWindGustDirectionVariance),
+        frame_id_(kDefaultFrameId),
+        pub_interval_(0.5),
+        node_handle_(NULL) {}
+
+  virtual ~GazeboWindShearPlugin();
+
+ protected:
+  /// \brief Load the plugin.
+  /// \param[in] _model Pointer to the model that loaded this plugin.
+  /// \param[in] _sdf SDF element that describes the plugin.
+  void Load(physics::ModelPtr model, sdf::ElementPtr sdf);
+
+  /// \brief Called when the world is updated.
+  /// \param[in] _info Update timing information.
+  void OnUpdate(const common::UpdateInfo& /*_info*/);
+
+ private:
+  /// \brief Pointer to the update event connection.
+  event::ConnectionPtr update_connection_;
+
+  physics::WorldPtr world_;
+  physics::ModelPtr model_;
+
+  std::string namespace_;
+
+  std::string frame_id_;
+  std::string wind_pub_topic_;
+
+  double wind_velocity_max_;
+  double wind_velocity_variance_;
+  double wind_gust_velocity_mean_;
+  double wind_gust_velocity_max_;
+  double wind_gust_velocity_variance_;
+  double pub_interval_;
+  std::default_random_engine wind_velocity_generator_;
+  std::normal_distribution<double> wind_velocity_distribution_;
+  std::default_random_engine wind_gust_velocity_generator_;
+  std::normal_distribution<double> wind_gust_velocity_distribution_;
+
+  ignition::math::Vector3d xyz_offset_;
+  ignition::math::Vector3d wind_direction_mean_;
+  ignition::math::Vector3d wind_gust_direction_mean_;
+  double wind_shear_gradient_{0.2}; // [m/s/m]Windspeed increase / altitude
+  double wind_shear_offset_{0.0}; // [m/s] Wind speed at 0m altitude
+  double wind_direction_variance_;
+  double wind_gust_direction_variance_;
+  std::default_random_engine wind_direction_generator_;
+  std::normal_distribution<double> wind_direction_distribution_X_;
+  std::normal_distribution<double> wind_direction_distribution_Y_;
+  std::normal_distribution<double> wind_direction_distribution_Z_;
+  std::default_random_engine wind_gust_direction_generator_;
+  std::normal_distribution<double> wind_gust_direction_distribution_X_;
+  std::normal_distribution<double> wind_gust_direction_distribution_Y_;
+  std::normal_distribution<double> wind_gust_direction_distribution_Z_;
+
+  common::Time wind_gust_end_;
+  common::Time wind_gust_start_;
+  common::Time last_time_;
+
+  transport::NodePtr node_handle_;
+  transport::PublisherPtr wind_pub_;
+
+  physics_msgs::msgs::Wind wind_msg;
+};
+}
+
+#endif // ROTORS_GAZEBO_PLUGINS_GAZEBO_WIND_PLUGIN_H
---
+++ src/gazebo_wind_shear_plugin.cpp
@@ -0,0 +1,169 @@
+/*
+ * Copyright 2015 Fadri Furrer, ASL, ETH Zurich, Switzerland
+ * Copyright 2015 Michael Burri, ASL, ETH Zurich, Switzerland
+ * Copyright 2015 Mina Kamel, ASL, ETH Zurich, Switzerland
+ * Copyright 2015 Janosch Nikolic, ASL, ETH Zurich, Switzerland
+ * Copyright 2015 Markus Achtelik, ASL, ETH Zurich, Switzerland
+ * Copyright 2016 Anton Matosov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "gazebo_wind_shear_plugin.h"
+#include "common.h"
+
+namespace gazebo {
+
+GazeboWindShearPlugin::~GazeboWindShearPlugin() {
+  update_connection_->~Connection();
+}
+
+void GazeboWindShearPlugin::Load(physics::ModelPtr model, sdf::ElementPtr sdf) {
+  model_ = model;
+
+  world_ = model->GetWorld();
+
+  double wind_gust_start = kDefaultWindGustStart;
+  double wind_gust_duration = kDefaultWindGustDuration;
+
+  if (sdf->HasElement("robotNamespace")) {
+    namespace_ = sdf->GetElement("robotNamespace")->Get<std::string>();
+  } else {
+    gzerr << "[gazebo_wind_plugin] Please specify a robotNamespace.\n";
+  }
+
+  node_handle_ = transport::NodePtr(new transport::Node());
+  node_handle_->Init(namespace_);
+
+  if (sdf->HasElement("xyzOffset"))
+    xyz_offset_ = sdf->GetElement("xyzOffset")->Get<ignition::math::Vector3d>();
+  else
+    gzerr << "[gazebo_wind_plugin] Please specify a xyzOffset.\n";
+
+  getSdfParam<std::string>(sdf, "windPubTopic", wind_pub_topic_, wind_pub_topic_);
+  double pub_rate = 2.0;
+  getSdfParam<double>(sdf, "publishRate", pub_rate, pub_rate); //Wind topic publishing rates
+  pub_interval_ = (pub_rate > 0.0) ? 1/pub_rate : 0.0;
+  getSdfParam<std::string>(sdf, "frameId", frame_id_, frame_id_);
+  // Get the wind params from SDF.
+  getSdfParam<double>(sdf, "windVelocityMax", wind_velocity_max_, wind_velocity_max_);
+  getSdfParam<double>(sdf, "windVelocityVariance", wind_velocity_variance_, wind_velocity_variance_);
+  getSdfParam<ignition::math::Vector3d>(sdf, "windDirectionMean", wind_direction_mean_, wind_direction_mean_);
+  getSdfParam<double>(sdf, "windDirectionVariance", wind_direction_variance_, wind_direction_variance_);
+  // Get the wind gust params from SDF.
+  getSdfParam<double>(sdf, "windGustStart", wind_gust_start, wind_gust_start);
+  getSdfParam<double>(sdf, "windGustDuration", wind_gust_duration, wind_gust_duration);
+  getSdfParam<double>(sdf, "windGustVeloctiyMean", wind_gust_velocity_mean_, wind_gust_velocity_mean_);
+  getSdfParam<double>(sdf, "windGustVelocityMax", wind_gust_velocity_max_, wind_gust_velocity_max_);
+  getSdfParam<double>(sdf, "windGustVelocityVariance", wind_gust_velocity_variance_, wind_gust_velocity_variance_);
+  getSdfParam<ignition::math::Vector3d>(sdf, "windGustDirectionMean", wind_gust_direction_mean_, wind_gust_direction_mean_);
+  getSdfParam<double>(sdf, "windGustDirectionVariance", wind_gust_direction_variance_, wind_gust_direction_variance_);
+  getSdfParam<double>(sdf, "windShearGradient", wind_shear_gradient_, wind_shear_gradient_);
+  getSdfParam<double>(sdf, "windShearOffset", wind_shear_offset_, wind_shear_offset_);
+
+  ///TODO: Define wind shear parameters
+
+
+  wind_direction_mean_.Normalize();
+  wind_gust_direction_mean_.Normalize();
+  wind_gust_start_ = common::Time(wind_gust_start);
+  wind_gust_end_ = common::Time(wind_gust_start + wind_gust_duration);
+  // Set random wind direction mean and standard deviation
+  wind_direction_distribution_X_.param(std::normal_distribution<double>::param_type(wind_direction_mean_.X(), sqrt(wind_direction_variance_)));
+  wind_direction_distribution_Y_.param(std::normal_distribution<double>::param_type(wind_direction_mean_.Y(), sqrt(wind_direction_variance_)));
+  wind_direction_distribution_Z_.param(std::normal_distribution<double>::param_type(wind_direction_mean_.Z(), sqrt(wind_direction_variance_)));
+  // Set random wind gust velocity mean and standard deviation
+  wind_gust_velocity_distribution_.param(std::normal_distribution<double>::param_type(wind_gust_velocity_mean_, sqrt(wind_gust_velocity_variance_)));
+  // Set random wind gust direction mean and standard deviation
+  wind_gust_direction_distribution_X_.param(std::normal_distribution<double>::param_type(wind_gust_direction_mean_.X(), sqrt(wind_gust_direction_variance_)));
+  wind_gust_direction_distribution_Y_.param(std::normal_distribution<double>::param_type(wind_gust_direction_mean_.Y(), sqrt(wind_gust_direction_variance_)));
+  wind_gust_direction_distribution_Z_.param(std::normal_distribution<double>::param_type(wind_gust_direction_mean_.Z(), sqrt(wind_gust_direction_variance_)));
+
+  // Listen to the update event. This event is broadcast every
+  // simulation iteration.
+  update_connection_ = event::Events::ConnectWorldUpdateBegin(boost::bind(&GazeboWindShearPlugin::OnUpdate, this, _1));
+
+  wind_pub_ = node_handle_->Advertise<physics_msgs::msgs::Wind>("~/" + wind_pub_topic_, 10);
+
+#if GAZEBO_MAJOR_VERSION >= 9
+  last_time_ = world_->SimTime();
+#else
+  last_time_ = world_->GetSimTime();
+#endif
+
+}
+
+// This gets called by the world update start event.
+void GazeboWindShearPlugin::OnUpdate(const common::UpdateInfo& _info) {
+  // Get the current simulation time.
+#if GAZEBO_MAJOR_VERSION >= 9
+  common::Time now = world_->SimTime();
+#else
+  common::Time now = world_->GetSimTime();
+#endif
+  if ((now - last_time_).Double() < pub_interval_ || pub_interval_ == 0.0) {
+    return;
+  }
+  last_time_ = now;
+
+  // Calculate the wind force.
+  // Get normal distribution wind strength
+#if GAZEBO_MAJOR_VERSION >= 9
+    ignition::math::Pose3d pose_model_world = model_->WorldPose();
+#else
+    ignition::math::Pose3d pose_model_world = ignitionFromGazeboMath(model_->GetWorldPose());
+#endif
+  double altitude = pose_model_world.Pos().Z();
+  ///TODO: get wind strength from altitude
+  ///Linear wind shear
+  double wind_shear_velocity = wind_shear_gradient_ * altitude + wind_shear_offset_;
+  // Set random wind velocity mean and standard deviation
+  wind_velocity_distribution_.param(std::normal_distribution<double>::param_type(wind_shear_velocity, sqrt(wind_velocity_variance_)));
+  double wind_strength = std::abs(wind_velocity_distribution_(wind_velocity_generator_));
+  wind_strength = (wind_strength > wind_velocity_max_) ? wind_velocity_max_ : wind_strength;
+  // Get normal distribution wind direction
+  ignition::math::Vector3d wind_direction;
+  wind_direction.X() = wind_direction_distribution_X_(wind_direction_generator_);
+  wind_direction.Y() = wind_direction_distribution_Y_(wind_direction_generator_);
+  wind_direction.Z() = wind_direction_distribution_Z_(wind_direction_generator_);
+  // Calculate total wind velocity
+  ignition::math::Vector3d wind = wind_strength * wind_direction;
+
+  ignition::math::Vector3d wind_gust(0, 0, 0);
+  // Calculate the wind gust velocity.
+  if (now >= wind_gust_start_ && now < wind_gust_end_) {
+    // Get normal distribution wind gust strength
+    double wind_gust_strength = std::abs(wind_gust_velocity_distribution_(wind_gust_velocity_generator_));
+    wind_gust_strength = (wind_gust_strength > wind_gust_velocity_max_) ? wind_gust_velocity_max_ : wind_gust_strength;
+    // Get normal distribution wind gust direction
+    ignition::math::Vector3d wind_gust_direction;
+    wind_gust_direction.X() = wind_gust_direction_distribution_X_(wind_gust_direction_generator_);
+    wind_gust_direction.Y() = wind_gust_direction_distribution_Y_(wind_gust_direction_generator_);
+    wind_gust_direction.Z() = wind_gust_direction_distribution_Z_(wind_gust_direction_generator_);
+    wind_gust = wind_gust_strength * wind_gust_direction;
+  }
+
+  gazebo::msgs::Vector3d* wind_v = new gazebo::msgs::Vector3d();
+  wind_v->set_x(wind.X() + wind_gust.X());
+  wind_v->set_y(wind.Y() + wind_gust.Y());
+  wind_v->set_z(wind.Z() + wind_gust.Z());
+
+  wind_msg.set_frame_id(frame_id_);
+  wind_msg.set_time_usec(now.Double() * 1e6);
+  wind_msg.set_allocated_velocity(wind_v);
+
+  wind_pub_->Publish(wind_msg);
+}
+
+GZ_REGISTER_MODEL_PLUGIN(GazeboWindShearPlugin);
+}
