402362bc601bad0d360bcb5f20304f625d86c5a3
Jaeyoung Lim <jalim@ethz.ch>
2022-08-31 16:45:19 +0200

    Traditional helicopter model
    Configure helicopter mesh
    
    
    Add helicopter visualizations
    
    
    Remove redundant rotors
    
    
    Fix tailrotor collision geometry
    
    
    Add swashplate plugin boiler plate code
    
    
    Fix
    
    
    Remove unnneded code
    
    
    Increase rotor effectiveness
    
    
    Add rolling moment
    
    
    Add torque
    
    
    Fix
    
    
    Fix
    
    
    F

---
+++ src/gazebo_swashplate_plugin.cpp
@@ -0,0 +1,209 @@
+/*
+ * Copyright 2015 Fadri Furrer, ASL, ETH Zurich, Switzerland
+ * Copyright 2015 Michael Burri, ASL, ETH Zurich, Switzerland
+ * Copyright 2015 Mina Kamel, ASL, ETH Zurich, Switzerland
+ * Copyright 2015 Janosch Nikolic, ASL, ETH Zurich, Switzerland
+ * Copyright 2015 Markus Achtelik, ASL, ETH Zurich, Switzerland
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#include "gazebo_swashplate_plugin.h"
+#include <ignition/math.hh>
+
+namespace gazebo {
+
+GazeboSwashplatePlugin::~GazeboSwashplatePlugin() {
+  updateConnection_->~Connection();
+  use_pid_ = false;
+}
+
+void GazeboSwashplatePlugin::InitializeParams() {}
+
+void GazeboSwashplatePlugin::Publish() {
+  turning_velocity_msg_.set_data(joint_->GetVelocity(0));
+  // FIXME: Commented out to prevent warnings about queue limit reached.
+  // motor_velocity_pub_->Publish(turning_velocity_msg_);
+}
+
+void GazeboSwashplatePlugin::Load(physics::ModelPtr _model, sdf::ElementPtr _sdf) {
+  model_ = _model;
+
+  namespace_.clear();
+
+  if (_sdf->HasElement("robotNamespace"))
+    namespace_ = _sdf->GetElement("robotNamespace")->Get<std::string>();
+  else
+    gzerr << "[gazebo_motor_model] Please specify a robotNamespace.\n";
+  node_handle_ = transport::NodePtr(new transport::Node());
+  node_handle_->Init(namespace_);
+
+  if (_sdf->HasElement("jointName"))
+    joint_name_ = _sdf->GetElement("jointName")->Get<std::string>();
+  else
+    gzerr << "[gazebo_motor_model] Please specify a jointName, where the rotor is attached.\n";
+  // Get the pointer to the joint.
+  joint_ = model_->GetJoint(joint_name_);
+  if (joint_ == NULL)
+    gzthrow("[gazebo_motor_model] Couldn't find specified joint \"" << joint_name_ << "\".");
+
+  // setup joint control pid to control joint
+  if (_sdf->HasElement("joint_control_pid"))
+  {
+    sdf::ElementPtr pid = _sdf->GetElement("joint_control_pid");
+    double p = 0.1;
+    if (pid->HasElement("p"))
+      p = pid->Get<double>("p");
+    double i = 0;
+    if (pid->HasElement("i"))
+      i = pid->Get<double>("i");
+    double d = 0;
+    if (pid->HasElement("d"))
+      d = pid->Get<double>("d");
+    double iMax = 0;
+    if (pid->HasElement("iMax"))
+      iMax = pid->Get<double>("iMax");
+    double iMin = 0;
+    if (pid->HasElement("iMin"))
+      iMin = pid->Get<double>("iMin");
+    double cmdMax = 3;
+    if (pid->HasElement("cmdMax"))
+      cmdMax = pid->Get<double>("cmdMax");
+    double cmdMin = -3;
+    if (pid->HasElement("cmdMin"))
+      cmdMin = pid->Get<double>("cmdMin");
+    pid_.Init(p, i, d, iMax, iMin, cmdMax, cmdMin);
+    use_pid_ = true;
+  }
+  else
+  {
+    use_pid_ = false;
+  }
+
+  if (_sdf->HasElement("linkName"))
+    link_name_ = _sdf->GetElement("linkName")->Get<std::string>();
+  else
+    gzerr << "[gazebo_motor_model] Please specify a linkName of the rotor.\n";
+  link_ = model_->GetLink(link_name_);
+  if (link_ == NULL)
+    gzthrow("[gazebo_motor_model] Couldn't find specified link \"" << link_name_ << "\".");
+
+
+  if (_sdf->HasElement("motorNumber"))
+    motor_number_ = _sdf->GetElement("motorNumber")->Get<int>();
+  else
+    gzerr << "[gazebo_motor_model] Please specify a motorNumber.\n";
+
+  if (_sdf->HasElement("turningDirection")) {
+    std::string turning_direction = _sdf->GetElement("turningDirection")->Get<std::string>();
+    if (turning_direction == "cw")
+      turning_direction_ = turning_direction::CW;
+    else if (turning_direction == "ccw")
+      turning_direction_ = turning_direction::CCW;
+    else
+      gzerr << "[gazebo_motor_model] Please only use 'cw' or 'ccw' as turningDirection.\n";
+  }
+  else
+    gzerr << "[gazebo_motor_model] Please specify a turning direction ('cw' or 'ccw').\n";
+
+
+  getSdfParam<std::string>(_sdf, "commandSubTopic", command_sub_topic_, command_sub_topic_);
+
+  getSdfParam<double>(_sdf, "rollingMomentCoefficient", rolling_moment_coefficient_,
+                      rolling_moment_coefficient_);
+  getSdfParam<double>(_sdf, "maxRotVelocity", max_rot_velocity_, max_rot_velocity_);
+  getSdfParam<double>(_sdf, "motorConstant", motor_constant_, motor_constant_);
+  getSdfParam<double>(_sdf, "momentConstant", moment_constant_, moment_constant_);
+
+  getSdfParam<double>(_sdf, "timeConstantUp", time_constant_up_, time_constant_up_);
+  getSdfParam<double>(_sdf, "timeConstantDown", time_constant_down_, time_constant_down_);
+  getSdfParam<double>(_sdf, "rotorVelocitySlowdownSim", rotor_velocity_slowdown_sim_, 10);
+
+  // Listen to the update event. This event is broadcast every
+  // simulation iteration.
+  updateConnection_ = event::Events::ConnectWorldUpdateBegin(boost::bind(&GazeboSwashplatePlugin::OnUpdate, this, _1));
+
+  command_sub_ = node_handle_->Subscribe<mav_msgs::msgs::CommandMotorSpeed>("~/" + model_->GetName() + command_sub_topic_, &GazeboSwashplatePlugin::VelocityCallback, this);
+  wind_sub_ = node_handle_->Subscribe("~/" + wind_sub_topic_, &GazeboSwashplatePlugin::WindVelocityCallback, this);
+
+  // Create the first order filter.
+  rotor_velocity_filter_.reset(new FirstOrderFilter<double>(time_constant_up_, time_constant_down_, ref_motor_rot_vel_));
+}
+
+// This gets called by the world update start event.
+void GazeboSwashplatePlugin::OnUpdate(const common::UpdateInfo& _info) {
+  sampling_time_ = _info.simTime.Double() - prev_sim_time_;
+  prev_sim_time_ = _info.simTime.Double();
+  UpdateForcesAndMoments();
+  Publish();
+}
+
+void GazeboSwashplatePlugin::VelocityCallback(CommandMotorSpeedPtr &rot_velocities) {
+  if(rot_velocities->motor_speed_size() < motor_number_) {
+    std::cout  << "You tried to access index " << motor_number_
+      << " of the MotorSpeed message array which is of size " << rot_velocities->motor_speed_size() << "." << std::endl;
+  } else {
+    ref_motor_rot_vel_ = std::min(static_cast<double>(rot_velocities->motor_speed(motor_number_)), static_cast<double>(max_rot_velocity_));
+    double swashplate_0 = static_cast<double>(rot_velocities->motor_speed(2));
+    double swashplate_1 = static_cast<double>(rot_velocities->motor_speed(3));
+    double swashplate_2 = static_cast<double>(rot_velocities->motor_speed(4));
+
+    ///Computing swashplate geometries
+    ///TODO: Define geometries more generally
+    ref_cyclic_command_ = (swashplate_0 + swashplate_1 + swashplate_2)/3;
+    ref_pitch_command_ = swashplate_0 - (std::cos(M_PI/3) * swashplate_1 + std::cos(M_PI/3) * swashplate_2)/2;
+    ref_roll_command_ = std::sin(M_PI/3) * swashplate_1 - std::sin(M_PI/3) * swashplate_2;
+
+  }
+}
+
+void GazeboSwashplatePlugin::MotorFailureCallback(const boost::shared_ptr<const msgs::Int> &fail_msg) {
+  motor_Failure_Number_ = fail_msg->data();
+}
+
+void GazeboSwashplatePlugin::UpdateForcesAndMoments() {
+  motor_rot_vel_ = joint_->GetVelocity(0);
+  if (motor_rot_vel_ / (2 * M_PI) > 1 / (2 * sampling_time_)) {
+    gzerr << "Aliasing on motor [" << motor_number_ << "] might occur. Consider making smaller simulation time steps or raising the rotor_velocity_slowdown_sim_ param.\n";
+  }
+  double real_motor_velocity = motor_rot_vel_ * rotor_velocity_slowdown_sim_;
+  double force = real_motor_velocity * std::abs(real_motor_velocity) * motor_constant_;
+
+  // Apply a force to the link.
+  link_->AddRelativeForce(ignition::math::Vector3d(0, 0, force));
+
+  ignition::math::Vector3d control_moment;
+  control_moment.X() = -ref_roll_command_ * 1.0;
+  control_moment.Y() = -ref_pitch_command_ * 1.0;
+  physics::Link_V parent_links = link_->GetParentJointsLinks();
+
+  ignition::math::Vector3d rolling_moment;
+  // - \omega * \mu_1 * V_A^{\perp}
+  // rolling_moment = -std::abs(real_motor_velocity) * turning_direction_ * rolling_moment_coefficient_ * velocity_perpendicular_to_rotor_axis;
+  parent_links.at(0)->AddTorque(control_moment);
+  // Apply the filter on the motor's velocity.
+  double ref_motor_rot_vel;
+  ref_motor_rot_vel = rotor_velocity_filter_->updateFilter(ref_motor_rot_vel_, sampling_time_);
+
+  joint_->SetVelocity(0, turning_direction_ * ref_motor_rot_vel / rotor_velocity_slowdown_sim_);
+}
+
+void GazeboSwashplatePlugin::WindVelocityCallback(WindPtr& msg) {
+  wind_vel_ = ignition::math::Vector3d(msg->velocity().x(),
+            msg->velocity().y(),
+            msg->velocity().z());
+}
+
+GZ_REGISTER_MODEL_PLUGIN(GazeboSwashplatePlugin);
+}
