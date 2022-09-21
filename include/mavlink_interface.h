93d3362ee5e507f433750dd4946fd54f16508502
Henry Kotze <henry@flycloudline.com>
2022-09-21 17:00:21 +0200

    Removed Old windsensor function

--- include/mavlink_interface.h
+++ include/mavlink_interface.h
@@ -170,7 +170,6 @@ public:
     void SendGpsMessages(const SensorData::Gps &data);
     void UpdateBarometer(const SensorData::Barometer &data, const int id = 0);
     void UpdateAirspeed(const SensorData::Airspeed &data, const int id = 0);
-    void UpdateWindSensor(const SensorData::WindSensor &data, const int id = 0);
     void UpdateIMU(const SensorData::Imu &data, const int id = 0);
     void UpdateMag(const SensorData::Magnetometer &data, const int id = 0);
     Eigen::VectorXd GetActuatorControls();
