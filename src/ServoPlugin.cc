9dd91867c1638dcc3e71bfb2fa6bfa95605fce59
Erik Bauer <erik.hamburg@gmx.de>
2023-03-23 15:42:42 +0100

    fixed reference access of SharedPtr

--- src/ServoPlugin.cc
+++ src/ServoPlugin.cc
@@ -23,9 +23,9 @@ ServoPlugin::~ServoPlugin() { delete joint_controller_; }
 
 void ServoPlugin::set_angle(const std_msgs::msg::Float64::SharedPtr msg) {
   if (td_ == false) {
-    angle_ = msg.data * M_PI / 180.0;
+    angle_ = msg->data * M_PI / 180.0;
   } else {
-    angle_ = -msg.data * M_PI / 180.0;
+    angle_ = -msg->data * M_PI / 180.0;
   }
 }
 
