0bc8342d959237f5e0e615c92c2b3d5e1335b600
Arman Raayatsanati <asanati@student.ethz.ch>
2023-03-24 16:33:49 +0100

    add compatibility with foxy

--- include/ServoPlugin.hh
+++ include/ServoPlugin.hh
@@ -47,7 +47,7 @@ private:
   // callbacks
   void set_angle(const std_msgs::msg::Float64::SharedPtr msg);
   void set_servo(
-      std::shared_ptr<raptor_interface::srv::SetServo::Request> request,
+      const std::shared_ptr<raptor_interface::srv::SetServo::Request> request,
       std::shared_ptr<raptor_interface::srv::SetServo::Response> response);
 
   // data pointer
