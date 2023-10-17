e909889141a095682ee28e2c300720cd71fb2739
Daniel Honies <daniel.honies@gmail.com>
2023-10-17 09:00:20 +0000

    Fix opencv aruco version bug

--- include/gazebo_aruco_plugin.h
+++ include/gazebo_aruco_plugin.h
@@ -39,7 +39,8 @@
 
 #include <opencv2/opencv.hpp>
 #include <opencv2/aruco.hpp>
-#include <opencv2/aruco/dictionary.hpp>
+#include <opencv2/objdetect/aruco_dictionary.hpp>
+//#include <opencv2/aruco/dictionary.hpp>
 #include <opencv2/core.hpp>
 
 #include <vector>
